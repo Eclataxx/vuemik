e462153a5b110d31d7bb63982d16e01abb9b5cee
Tom Délié <tomdelie78@gmail.com>
2020-12-10 15:14:59 +0100

    Fix dependencies

--- src/vuemik/Vuemik.vue
+++ src/vuemik/Vuemik.vue
@@ -1,57 +1,59 @@
 <template>
-  <div class="vuemik-form" >
+  <div class="vuemik-form">
     <slot :handleSubmit="handleSubmit"></slot>
   </div>
 </template>
 
 <script>
-import { vueSet as set } from "vue-deepset";
 export default {
-  name: "Vuemik",
+  name: 'Vuemik',
   props: {
     onSubmit: { type: Function, required: true },
-    initialValues: { type: Object, required: true }
+    initialValues: { type: Object, required: true },
   },
   data() {
     return {
-      values: JSON.parse(JSON.stringify(this.initialValues))
+      values: JSON.parse(JSON.stringify(this.initialValues)),
     };
   },
   provide() {
     return {
       vuemik: {
         values: this.values,
-        change: this.handleChange
-      }
+        change: this.handleChange,
+      },
     };
   },
   methods: {
     eventOrValue(e) {
-      if (e instanceof Event) {
-        if (e.target.options) {
-          const selectedOption = e.target.options[e.target.selectedIndex];
-          return selectedOption._value !== undefined
-            ? selectedOption._value
-            : selectedOption.value;
-        }
-        if (e.target.type === "checkbox") {
-          return e.target.checked;
-        }
-        return e.target.value;
+      if (!(e instanceof Event)) {
+        throw new Error("Instance of Event expected");
       }
-      return e;
+
+      if (e.target.options) {
+        const selectedOption = e.target.options[e.target.selectedIndex];
+        return selectedOption._value !== undefined
+          ? selectedOption._value
+          : selectedOption.value;
+      }
+
+      if (e.target.type === "checkbox") {
+        return e.target.checked;
+      }
+
+      return e.target.value;
     },
     handleChange(e) {
       this.setValues({ [e.target.name]: this.eventOrValue(e) });
     },
     setValues(values) {
       Object.entries(values).forEach(([key, val]) => {
-        set(this.values, key, val);
+        this.$data.values[key] = val; 
       });
     },
     handleSubmit() {
       this.onSubmit(this.values);
-    }
-  }
+    },
+  },
 };
 </script>
